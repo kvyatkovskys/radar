c004cbad2742610e6009fb0d982b123765b7c21a
Kviatkovskii <sergeikviatkovskii@gmail.com>
2017-12-24 16:39:27 +0300

    detect user location's

--- Circle/MainViewController.swift
+++ Circle/MainViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 import SnapKit
 import MapKit
 
-final class MainViewController: UIViewController {
-    
+final class MainViewController: UIViewController, LocationManagerDelegate {
     typealias Dependecies = HasRouter
     
+    // для работы с геопозиции
+    fileprivate lazy var locationsManager: LocationManager = {
+        return LocationManager(delegate: self)
+    }()
+    
     fileprivate let router: Router
+    
+    fileprivate lazy var tableView: UITableView = {
+        let table = UITableView()
+        //table.tableFooterView = UIView(frame: CGRect.zero)
+        return table
+    }()
+    
+    fileprivate lazy var headerView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100.0))
+        view.backgroundColor = .white
+        return view
+    }()
     
     fileprivate lazy var mapView: MKMapView = {
         let map = MKMapView()
@@ -31,6 +47,10 @@ final class MainViewController: UIViewController {
     }()
     
     fileprivate func updateConstraints() {
+        tableView.snp.makeConstraints { (make) in
+            make.top.bottom.left.right.equalToSuperview()
+        }
+        
         mapView.snp.makeConstraints { (make) in
             make.left.top.bottom.right.equalToSuperview()
         }
@@ -49,15 +69,50 @@ final class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(mapView)
+        
+        headerView.addSubview(mapView)
+        view.addSubview(tableView)
+        tableView.tableHeaderView = headerView
         
         updateConstraints()
-        
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-        centerMapOnLocation(location: initialLocation)
+        startDetectLocation()
     }
     
-    func centerMapOnLocation(location: CLLocation) {
+    // MARK: LocationManagerDelegate
+    func startDetectLocation() {
+        locationsManager.start { [unowned self] (start) in
+            if !start {
+                let alertController = UIAlertController(
+                    title: "Доступ к местоположению отключен",
+                    message: "Чтобы определить местоположение автоматически, откройте настройки этого приложения и установите для него доступ 'При использовании приложения'.",
+                    preferredStyle: .alert)
+                
+                let cancelAction = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                
+                let openAction = UIAlertAction(title: "Открыть Настройки", style: .default) { _ in
+                    if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+                alertController.addAction(openAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func locationManager(didFailWithError error: Error) {
+        showAlertLight(title: "Ошибка", message: "Мы не можем определить ваше местоположение!")
+    }
+    
+    func locationManager(currentLocation: Location?) {
+        if let latitude = currentLocation?.latitude, let longitude = currentLocation?.longitude {
+            centerMapOnLocation(CLLocation(latitude: latitude, longitude: longitude))
+        }
+    }
+    
+    func centerMapOnLocation(_ location: CLLocation) {
         let regionRadius: CLLocationDistance = 1000
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                   regionRadius, regionRadius)
---
+++ Circle/QuickAlertController.swift
@@ -0,0 +1,29 @@
+//
+//  QuickAlertController.swift
+//  Circle
+//
+//  Created by Kviatkovskii on 24/12/2017.
+//  Copyright © 2017 Kviatkovskii. All rights reserved.
+//
+
+import UIKit
+
+struct TitleButtonAlert {
+    static let buttonOK = "Готово"
+    static let buttonCancel = "Отменить"
+}
+
+extension UIViewController {
+    func showAlertLight(title: String, message: String, buttonTitle: String = TitleButtonAlert.buttonOK) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        let button = UIAlertAction(title: buttonTitle,
+                                   style: UIAlertActionStyle.default, handler: nil)
+        alert.addAction(button)
+        
+        DispatchQueue.main.async { [unowned self] in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
