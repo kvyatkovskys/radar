993a299620aa517ab79aa8af60ddb2d907de50b5
Kviatkovskii <sergeikviatkovskii@gmail.com>
2018-02-16 11:34:16 +0300

    run test again

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [["$TRAVIS_BRANCH" == "develop"]]; then
+if [ "$TRAVIS_BRANCH" = "develop" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	fastlane test
 	exit $?
 fi
 
-if [["$TRAVIS_PULL_REQUEST" != false]]; then
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "true" ]; then
 	fastlane test
 	exit $?
 fi	
